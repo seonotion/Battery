29f604510c98af97ae686e04284fb423d9a43fa5
seonotion <patel.avkash@gmail.com>
2015-03-19 23:40:11 +0530

    updated percentage

--- app/src/main/java/com/battery/MainActivity.java
+++ app/src/main/java/com/battery/MainActivity.java
@@ -4,18 +4,74 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.ViewFlipper;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    TextView tvPerc;
+    private Timer timer = new Timer();
+    private TimerTask timerTask;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) throws IllegalStateException {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         final ViewFlipper flipper = (ViewFlipper)findViewById(R.id.flipper);
         flipper.startFlipping();
+
+        tvPerc=(TextView)findViewById(R.id.tvPerc);
+
+tvPerc.setText("20");
+
+       /* timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                //refresh your textview
+                Integer per = Integer.parseInt(tvPerc.getText().toString());
+                if (per < 100) {
+                    per = per + 20;
+                } else {
+                    per = 0;
+                }
+                tvPerc.setText(per.toString());
+            }
+        };
+        timer.schedule(timerTask, 0, 1000);*/
+
+        Thread t = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    while (!isInterrupted()) {
+                        Thread.sleep(1000);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                // update TextView here!
+                                Integer per = Integer.parseInt(tvPerc.getText().toString());
+                                if (per < 100) {
+                                    per = per + 20;
+                                } else {
+                                    per = 20;
+                                }
+                                tvPerc.setText(per.toString());
+                            }
+                        });
+                    }
+                } catch (InterruptedException e) {
+                }
+            }
+        };
+
+        t.start();
+
+
     }
 
 
